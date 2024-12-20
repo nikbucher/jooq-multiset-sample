ad1fb9d7503bee00089503c593d976af564a4920
Niklaus Bucher <niklaus@bucher.io>
2024-12-20 01:02:28 +0100

    fixed implementation and improved test data

--- src/test/java/io/bucher/sample/JooqMultisetaggSampleApplicationTests.java
+++ src/test/java/io/bucher/sample/JooqMultisetaggSampleApplicationTests.java
@@ -3,12 +3,16 @@ package io.bucher.sample;
 import static io.bucher.sample.Tables.SOME_ENTRY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.jooq.Records.mapping;
-import static org.jooq.impl.DSL.multisetAgg;
+import static org.jooq.impl.DSL.field;
+import static org.jooq.impl.DSL.multiset;
+import static org.jooq.impl.DSL.name;
+import static org.jooq.impl.DSL.select;
 import static org.jooq.impl.DSL.sum;
 
 import io.bucher.sample.domain.Day;
 import io.bucher.sample.domain.DaySummary;
 import io.bucher.sample.domain.Entry;
+import java.time.LocalDate;
 import java.util.List;
 import org.jooq.DSLContext;
 import org.junit.jupiter.api.Test;
@@ -37,21 +41,32 @@ class JooqMultisetaggSampleApplicationTests {
 				Day.from("2024-01-02"),
 				Day.from("2024-01-03"),
 				Day.from("2024-01-04"),
-				Day.from("2024-01-05"),
-				Day.from("2024-01-06"),
-				Day.from("2024-01-07")
+				Day.from("2024-01-05")
 		);
 	}
 
 	@Test
 	void should_fetch_daily_entries() {
 		var e = SOME_ENTRY.as("e");
+		var daySummary = name("day_summary");
+
+		var daySummaryCte = ctx.with(daySummary).as(
+				select(
+						e.DATE.as("date"),
+						e.DESCRIPTION.as("description"),
+						sum(e.DURATION).as("duration")
+				).from(e)
+						.groupBy(e.DATE, e.DESCRIPTION)
+		);
 
 		List<DaySummary> result = ctx.select(
 						e.DATE,
-						multisetAgg(
-								e.DESCRIPTION,
-								sum(e.DURATION).cast(long.class)
+						multiset(
+								daySummaryCte.select(
+												field("description", String.class),
+												field("duration", Long.class)
+										).from(daySummary)
+										.where(field("day_summary.date", LocalDate.class).eq(e.DATE))
 						).convertFrom(r -> r.map(mapping(Entry::new)))
 				)
 				.from(e)
@@ -59,7 +74,37 @@ class JooqMultisetaggSampleApplicationTests {
 				.orderBy(e.DATE)
 				.fetch(mapping(DaySummary::new));
 
-		assertThat(result).hasSize(7);
+		assertThat(result).containsExactly(
+				new DaySummary(LocalDate.parse("2024-01-01"),
+						List.of(
+								new Entry("Task A description", 90),
+								new Entry("Task B description", 120),
+								new Entry("Task C description", 180)
+						)
+				),
+				new DaySummary(LocalDate.parse("2024-01-02"),
+						List.of(
+								new Entry("Task B description", 120),
+								new Entry("Task C description", 150)
+						)
+				),
+				new DaySummary(LocalDate.parse("2024-01-03"),
+						List.of(
+								new Entry("Task A description", 310),
+								new Entry("Task C description", 90)
+						)
+				),
+				new DaySummary(LocalDate.parse("2024-01-04"),
+						List.of(
+								new Entry("Task C description", 100)
+						)
+				),
+				new DaySummary(LocalDate.parse("2024-01-05"),
+						List.of(
+								new Entry("Task B description", 390)
+						)
+				)
+		);
 
 	}
 
